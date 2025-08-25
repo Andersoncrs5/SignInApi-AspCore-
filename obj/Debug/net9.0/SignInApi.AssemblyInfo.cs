9b8b57266f5851ed661caa5fe5b294dcf7df3b0b
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-25 16:09:08 -0300

    new packages

--- obj/Debug/net9.0/SignInApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/SignInApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignInApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07578a2f2a57d88967292ca2e123efc94d48624b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8582e5c35932040d84148e4a79f3d3e44aa4d8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignInApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignInApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
